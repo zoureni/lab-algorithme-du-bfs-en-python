3c9064ffffeae7b6cc7470129cc9378fa43d0de2
zoureni <zoureni@outlook.fr>
2020-04-01 14:06:21 -0400

    La taille de la queue est <14>

--- b000000000.py
+++ b000000000.py
@@ -4,8 +4,26 @@
 @author: zoureni
 """
 
-def main():
-  print('Informatique: le rêve')
+eleves = {}
+eleves["Boris"]=["Amir","Franck","Nathalie","Bertrand"]
+eleves["Amir"]=[]
+eleves["Franck"]=[]
+eleves["Nathalie"]=[]
+eleves["Bertrand"]=["Erna","Hassana","Abdelkrim"]
+eleves["Erna"]=[]
+eleves["Hassana"]=[]
+eleves["Zoureni"]=["Sekou","Auriane","Corlings"]
+eleves["Sekou"]=[]
+eleves["Auriane"]=[]
+eleves["Corlings"]=[]
+eleves["Abdelkrim"]=["Souleyman","Zack","Zoureni"]
+eleves["Souleyman"]=[]
+eleves["Zack"]=[]
+
+def search(name):
+   print( len(eleves.values()) )
+   return False
 
 if __name__== "__main__":
-  main()
+  search("Boris")
+
