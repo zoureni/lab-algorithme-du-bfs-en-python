90c70bed3dcf66fdb28cdd30cb3e78cf3596fc21
zoureni <zoureni@outlook.fr>
2020-04-03 11:50:06 -0400

    zoureni a le fameux Mac

--- b000000000.py
+++ b000000000.py
@@ -20,10 +20,26 @@ eleves["Abdelkrim"]=["Souleyman","Zack","Zoureni"]
 eleves["Souleyman"]=[]
 eleves["Zack"]=[]
 
+def personne_elue(name):
+   return name == 'Zoureni'
+   
+from collections import deque
+
 def search(name):
    print( len(eleves.values()) )
+   search_queue = deque()
+   search_queue += eleves[name]
+   print( len(search_queue) )
+   while search_queue:
+      personne = search_queue.popleft()
+      if personne_elue(personne):
+         print(personne + " a le fameux Mac")
+         return True
+      search_queue += eleves[personne]
    return False
 
 if __name__== "__main__":
-  search("Boris")
+   search("Boris")
+  
+
 
